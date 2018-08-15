41e60fb6f53ce45ade16f43725cf640c6cf08ddc
Unknown <gregorysl@gmail.com>
2018-08-15 15:11:08 +0200

    Add changes to class for export

--- ReleaseNotes/Program.cs
+++ ReleaseNotes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.TeamFoundation.Client;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
@@ -32,10 +33,10 @@ namespace ReleaseNotes
                 ? VersionSpec.Latest
                 : new ChangesetVersionSpec(changesetTo);
 
-            var path = $"c:\\PatchNotes\\{tfsBranchName}\\";
+            var path = $"c:\\PatchNotes\\{tfsBranchName}\\"; 
             Directory.CreateDirectory(path);
 
-            var excludedDirs = new[] {"CodeAnalysis", "QA", "_AutomatedBuild"};
+            var excludedDirs = new[] { "CodeAnalysis", "QA", "_AutomatedBuild" };
             var categories = new[] { "UIFramework", "Framework", "Infrastructure", "WebApp", "PSSolution" };
 
             var configurationServer = TfsConfigurationServerFactory.GetConfigurationServer(new Uri(tfsUrl));
@@ -45,44 +46,54 @@ namespace ReleaseNotes
             var svc = collection.GetService<VersionControlServer>();
 
             var list = svc.QueryHistory(queryLocation, VersionSpec.Latest, 0, RecursionType.Full, null, versionSpecFrom,
-                versionSpecTo, Int32.MaxValue, true, false).Cast<Changeset>().OrderBy(x => x.ChangesetId).ToList();
+                versionSpecTo, Int32.MaxValue, true, false).OfType<Changeset>().OrderBy(x => x.ChangesetId).ToList();
             var changes = list.Where(x => x.CommitterDisplayName != "TFS Service")
                 .OrderBy(x => x.ChangesetId).ToList();
+
+            var categoryChangesList = new List<CategoryChanges>();
+
             foreach (var category in categories)
             {
-                var categorySb = new StringBuilder();
+                var categoryChanges = new CategoryChanges {Name = category};
                 var categoryPath = $"$/{tfsProjectName}/{tfsBranchName}/{category}";
                 var catList = changes.Where(x => x.Changes.Any(c => c.Item.ServerItem.Contains(categoryPath)));
                 foreach (var item in catList)
                 {
-                    var changesetInfo = $"{item.ChangesetId},{item.CommitterDisplayName},{item.CreationDate},{item.Comment}";
+                    var changesetInfo = new ChangesetInfo
+                    {
+                        Id = item.ChangesetId,
+                        CommitedBy = item.CommitterDisplayName,
+                        Created = item.CreationDate,
+                        Comment = item.Comment
+                    };
                     var workItemWithoutCodeReview = item.AssociatedWorkItems.Where(x => x.WorkItemType != "Code Review Request").ToList();
                     if (!workItemWithoutCodeReview.Any())
                     {
-                        categorySb.Append($"{changesetInfo},N/A,N/A\n");
+                        changesetInfo.WorkItemId = "N/A";
+                        changesetInfo.WorkItemTitle = "N/A";
+
+                        categoryChanges.Changes.Add(changesetInfo);
                     }
                     foreach (var info in workItemWithoutCodeReview)
                     {
-                        categorySb.Append($"{changesetInfo},{info.Id},{info.Title}\n");
-                    }
 
+                        changesetInfo.WorkItemId = info.Id.ToString();
+                        changesetInfo.WorkItemTitle = info.Title;
+                        categoryChanges.Changes.Add(changesetInfo);
+                    }
                 }
 
-                if (categorySb.Length != 0)
+                if (categoryChanges.Changes.Count != 0)
                 {
-                    var savePath = $"{path}\\{category}_{DateTime.Now.ToFileTime()}.csv";
-                    using (var writetext = new StreamWriter(savePath))
-                    {
-                        writetext.Write(categorySb.ToString());
-                    }
+                    categoryChangesList.Add(categoryChanges);
                 }
             }
 
-            var workItems = changes.SelectMany(x => x.WorkItems).ToList().Where(x=>x.Type.Name != "Code Review Request").GroupBy(x=>x.Id).Select(x=>x.First()).OrderBy(x=>x.Id).ToList();
+            var workItems = changes.SelectMany(x => x.WorkItems).ToList().Where(x => x.Type.Name != "Code Review Request").GroupBy(x => x.Id).Select(x => x.First()).OrderBy(x => x.Id).ToList();
             //WorkItemStore workItemStore = new WorkItemStore(collection);
             //workItemStore.
             //var sv1c = collection.GetService<Microsoft.TeamFoundation.>();
-            var asddadddddd = workItems.Where(HasClientProject).OrderBy(x=> x.Fields["client.project"].Value).ToList();
+            var asddadddddd = workItems.Where(HasClientProject).OrderBy(x => x.Fields["client.project"].Value).ToList();
 
             var projectItemsSb = new StringBuilder();
             asddadddddd.ForEach(x => projectItemsSb.Append($"{x.Id} {x.Title} {x.Fields["client.project"].Value}\n"));
@@ -117,5 +128,26 @@ namespace ReleaseNotes
         //    var excludedDirs = new[] { "QA", "_AutomatedBuild"};
         //    return list.Any(x => excludedDirs.Any(x.Item.ServerItem.Contains));
         //}
+    }
+
+    public class CategoryChanges
+    {
+        public string Name { get; set; }
+        public List<ChangesetInfo> Changes { get; set; } = new List<ChangesetInfo>();
+    }
+
+    public class ChangesetInfo
+    {
+        //{item.ChangesetId},{item.CommitterDisplayName},{item.CreationDate},{item.Comment},{info.Id},{info.Title}
+        public int Id { get; set; }
+        public string CommitedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string Comment { get; set; }
+        public string WorkItemId { get; set; }
+        public string WorkItemTitle { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}-{WorkItemTitle}-{WorkItemId}";
+        }
     }
 }
