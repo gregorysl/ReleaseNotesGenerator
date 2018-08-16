00d9b3ffa38d047f75e8c10b9f9d99729b1f9c05
Unknown <gregorysl@gmail.com>
2018-08-16 15:03:46 +0200

    Add project for connecting with tfs

---
+++ TfsData/TfsConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace TfsData
+{
+    public class TfsConnector
+    {
+        private readonly WorkItemStore _itemStore;
+        private readonly VersionControlServer _changesetServer;
+        private TfsTeamProjectCollection _tfsTeamProjectCollection;
+
+        public TfsConnector(string url)
+        {
+            _tfsTeamProjectCollection = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(url, UriKind.Absolute));
+            _itemStore = _tfsTeamProjectCollection.GetService<WorkItemStore>();
+            _changesetServer = _tfsTeamProjectCollection.GetService<VersionControlServer>();
+        }
+
+    }
+}
