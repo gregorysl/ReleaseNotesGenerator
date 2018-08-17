18bad9de2ef5ac7bc3e4ddc9b25f9d887d5cc83c
Unknown <gregorysl@gmail.com>
2018-08-17 13:56:05 +0200

    Connectiong to tfs and getting iterations and release name

--- Gui/MainWindow.xaml.cs
+++ Gui/MainWindow.xaml.cs
@@ -1,19 +1,37 @@
 ﻿using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using TfsData;
 using Xceed.Words.NET;
 
 namespace Gui
 {
     public partial class MainWindow
     {
+        private readonly string _regexString = @".*\\\w+(?:.*)?\\((\w\d.\d+.\d+).\d+)";
+        public static TfsConnector _tfs;
         public MainWindow()
         {
-            var categories = new[] { "UIFramework", "Framework", "Infrastructure", "WebApp", "PSSolution" };
+            //DoStuff();
+
+            InitializeComponent();
+            //Close();
+        }
+
+        private static void DoStuff()
+        {
+            var categories = new[] {"UIFramework", "Framework", "Infrastructure", "WebApp", "PSSolution"};
             var dTestDocx = @"D:\test.docx";
             string fileName = @"D:\Template.docx";
 
             using (var doc = DocX.Load(fileName))
             {
+                //doc.ReplaceText(Tokens.ReleaseNumber, releaseName);
+
                 var secondSection = doc.Paragraphs.FirstOrDefault(x => x.Text == "Code Change sets in this Release");
                 var paragraph = secondSection.InsertParagraphAfterSelf("asd").FontSize(10d);
                 InsertBeforeOrAfter placeholder = paragraph;
@@ -46,10 +64,54 @@ namespace Gui
             }
 
             Process.Start(dTestDocx);
-            
-            InitializeComponent();
-            Close();
         }
 
+        private void ConnectTfsButton(object sender, RoutedEventArgs e)
+        {
+            _tfs = new TfsConnector(TfsUrl.Text);
+
+            if (!_tfs.IsConnected) return;
+            ProjectStack.Visibility = Visibility.Visible;
+            ProjectCombo.ItemsSource = _tfs.Projects;
+        }
+
+        private void ProjectSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (ProjectCombo.SelectedIndex == -1) return;
+            IterationStack.Visibility = Visibility.Visible;
+            var projectName = ProjectCombo.SelectedItem.ToString();
+            var iterationPaths = _tfs.GetIterationPaths(projectName);
+
+            
+            var regex = new Regex(_regexString);
+            var filtered = iterationPaths.Where(x => regex.IsMatch(x)).ToList();
+
+            IterationCombo.ItemsSource = filtered;
+
+
+        }
+
+        private void IterationSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (IterationCombo.SelectedIndex == -1) return;
+            var iteration = IterationCombo.SelectedItem.ToString();
+            var regex = new Regex(_regexString);
+            var a = regex.Match(iteration).Groups;
+            if (a.Count == 3)
+            {
+                ReleaseName.Text = a[1].Value;
+                Branch.Text = a[2].Value;
+            }
+            else
+            {
+                ReleaseName.Text = "";
+                Branch.Text = a[1].Value;
+            }
+        }
+
+        private void ConvertClicked(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
