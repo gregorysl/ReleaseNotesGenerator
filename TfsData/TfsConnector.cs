88747cf6e61d3b5131befe4177ceded9a0366f88
Unknown <gregorysl@gmail.com>
2018-08-17 13:54:50 +0200

    getting change sets and work items

--- TfsData/TfsConnector.cs
+++ TfsData/TfsConnector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
@@ -11,10 +12,23 @@ namespace TfsData
     {
         private readonly WorkItemStore _itemStore;
         private readonly VersionControlServer _changesetServer;
-        private TfsTeamProjectCollection _tfsTeamProjectCollection;
+        private readonly TfsTeamProjectCollection _tfsTeamProjectCollection;
+        private string _workItemsForIteration = "SELECT [System.Id], [System.Title] FROM WorkItems WHERE [System.IterationPath] UNDER '{0}'";
+        private string _workItemsByIds = "SELECT * FROM WorkItems WHERE [System.Id] in ({0})";
 
         public TfsConnector(string url)
         {
+            //var versionSpecFrom = changesetFrom.IsNullOrEmpty()
+            //    ? new ChangesetVersionSpec(1)
+            //    : new ChangesetVersionSpec(changesetFrom);
+
+            //var versionSpecTo = changesetTo.IsNullOrEmpty()
+            //    ? VersionSpec.Latest
+            //    : new ChangesetVersionSpec(changesetTo);
+
+            //var excludedDirs = new[] { "CodeAnalysis", "QA", "_AutomatedBuild" };
+            //var categories = new[] { "UIFramework", "Framework", "Infrastructure", "WebApp", "PSSolution" };
+
             _tfsTeamProjectCollection = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(url, UriKind.Absolute));
             _itemStore = _tfsTeamProjectCollection.GetService<WorkItemStore>();
             _changesetServer = _tfsTeamProjectCollection.GetService<VersionControlServer>();
@@ -36,10 +50,13 @@ namespace TfsData
                 result.Add(node.Path);
                 RecursiveAddIterationPath(node, result);
             }
-
             return result;
         }
 
+        public string GetChangesetTitleById(int id)
+        {
+            return _changesetServer.GetChangeset(id).Comment;
+        }
         private static void RecursiveAddIterationPath(Node node, ICollection<string> result)
         {
             foreach (Node item in node.ChildNodes)
@@ -54,15 +71,85 @@ namespace TfsData
 
         public List<string> GetWorkItemsInIterationPath(string iterationPath)
         {
-            List<string> result = new List<string>();
-            string query =
-                $"SELECT [System.Id], [System.Title] FROM WorkItems WHERE [System.IterationPath] UNDER '{iterationPath}'";
-            foreach (WorkItem item in _itemStore.Query(query))
+            var workItems = _itemStore.Query(string.Format(_workItemsForIteration, iterationPath)).Cast<WorkItem>().ToList();
+            var result = new List<string>();
+            foreach (var item in workItems)
             {
                 result.Add(item.Fields["System.Id"].Value + ":" + item.Fields["System.Title"].Value);
             }
 
             return result;
+        }
+
+        public List<Changeset> GetChangesets(string queryLocation, VersionSpec versionSpecFrom, VersionSpec versionSpecTo)
+        {
+            var list = _changesetServer.QueryHistory(queryLocation, VersionSpec.Latest, 0, RecursionType.Full, null, versionSpecFrom,
+                versionSpecTo, int.MaxValue, true, false).OfType<Changeset>().OrderBy(x => x.ChangesetId).ToList();
+            return list.Where(x => x.CommitterDisplayName != "TFS Service").ToList();
+        }
+
+        public List<Model.WorkItemDetails> GetWorkItemsFromChangesets(List<Changeset> changes)
+        {
+            var workItemIds = changes.SelectMany(x => x.AssociatedWorkItems).ToList().Select(x => x.Id.ToString()).Distinct().ToList();
+            var joinedWorkItems = string.Join(",", workItemIds);
+            var workItems = _itemStore.Query(String.Format(_workItemsByIds, joinedWorkItems));
+
+            var workItemChanges = new List<Model.WorkItemDetails>();
+            foreach (WorkItem workItem in workItems)
+            {
+                workItemChanges.Add(new Model.WorkItemDetails
+                {
+                    Id = workItem.Id,
+                    Title = workItem.Title,
+                    ClientProject = workItem.Fields["client.project"].Value.ToString()
+                });
+            }
+
+            return workItemChanges;
+        }
+
+
+        public void GetCategorizedChanges(List<Changeset> changes, List<string> categories)
+        {
+
+            //var categoryChangesList = new List<Model.CategoryChanges>();
+
+            //foreach (var category in categories)
+            //{
+            //    var categoryChanges = new Model.CategoryChanges { Name = category };
+            //    var categoryPath = $"$/{tfsProjectName}/{tfsBranchName}/{category}";
+            //    var catList = changes.Where(x => x.Changes.Any(c => c.Item.ServerItem.Contains(categoryPath)));
+            //    foreach (var item in catList)
+            //    {
+            //        var changesetInfo = new Model.ChangesetInfo
+            //        {
+            //            Id = item.ChangesetId,
+            //            CommitedBy = item.CommitterDisplayName,
+            //            Created = item.CreationDate,
+            //            Comment = item.Comment
+            //        };
+            //        var workItemWithoutCodeReview = item.AssociatedWorkItems.Where(x => x.WorkItemType != "Code Review Request").ToList();
+            //        if (!workItemWithoutCodeReview.Any())
+            //        {
+            //            changesetInfo.WorkItemId = "N/A";
+            //            changesetInfo.WorkItemTitle = "N/A";
+
+            //            categoryChanges.Changes.Add(changesetInfo);
+            //        }
+            //        foreach (var info in workItemWithoutCodeReview)
+            //        {
+
+            //            changesetInfo.WorkItemId = info.Id.ToString();
+            //            changesetInfo.WorkItemTitle = info.Title;
+            //            categoryChanges.Changes.Add(changesetInfo);
+            //        }
+            //    }
+
+            //    if (categoryChanges.Changes.Count != 0)
+            //    {
+            //        categoryChangesList.Add(categoryChanges);
+            //    }
+            //}
         }
     }
 }
