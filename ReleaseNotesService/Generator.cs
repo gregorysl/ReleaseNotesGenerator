4e3c0f18bbac52a5e0b5b08ab6a69181dfffbcb8
gregorysl <gregorysl@gmail.com>
2020-05-05 13:15:23 +0200

    format generator

--- ReleaseNotesService/Generator.cs
+++ ReleaseNotesService/Generator.cs
@@ -1,55 +1,59 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DataModel;
 using TfsData;
 
-namespace ReleaseNotesService {
-    public class Generator {
-
+namespace ReleaseNotesService
+{
+    public class Generator
+    {
         private readonly TfsConnector _tfs;
 
-        public Generator (TfsConnector tfs) {
+        public Generator(TfsConnector tfs)
+        {
             _tfs = tfs;
         }
 
-        public string CreateDoc(DownloadedItems downloadedData, List<string> workItemStateInclude, ReleaseData releaseData, string documentLocation) {
+        public string CreateDoc(DownloadedItems downloadedData, List<string> workItemStateInclude, ReleaseData releaseData, string documentLocation)
+        {
             var selectedChangesets = downloadedData.Changes
-                .Where (x => x.Selected)
-                .OrderBy (x => x.changesetId)
-                .Select (item => new ChangesetInfo {
+                .Where(x => x.Selected)
+                .OrderBy(x => x.changesetId)
+                .Select(item => new ChangesetInfo
+                {
                     Id = item.changesetId,
-                        Comment = item.comment,
-                        CommitedBy = item.checkedInBy.displayName,
-                        Created = item.createdDate
-                }).ToList ();
+                    Comment = item.comment,
+                    CommitedBy = item.checkedInBy.displayName,
+                    Created = item.createdDate
+                }).ToList();
 
             var categories = downloadedData.Categorized
-                .Where (category => selectedChangesets.Any (x => category.Value.Contains (x.Id))).ToDictionary (
+                .Where(category => selectedChangesets.Any(x => category.Value.Contains(x.Id))).ToDictionary(
                     category => category.Key,
-                    category => selectedChangesets.Where (x => category.Value.Contains (x.Id)).ToList ());
+                    category => selectedChangesets.Where(x => category.Value.Contains(x.Id)).ToList());
 
-            var workItems = downloadedData.WorkItems.Where (x => workItemStateInclude.Contains (x.State) && x.ClientProject != null)
-                .OrderBy (x => x.ClientProject);
-            var pbi = downloadedData.WorkItems.Where (x => workItemStateInclude.Contains (x.State) && x.ClientProject == null)
-                .OrderBy (x => x.Id);
-            var message = new DocumentEditor ().ProcessData (documentLocation, releaseData, categories, workItems, pbi);
+            var workItems = downloadedData.WorkItems.Where(x => workItemStateInclude.Contains(x.State) && x.ClientProject != null)
+                .OrderBy(x => x.ClientProject);
+            var pbi = downloadedData.WorkItems.Where(x => workItemStateInclude.Contains(x.State) && x.ClientProject == null)
+                .OrderBy(x => x.Id);
+            var message = new DocumentEditor().ProcessData(documentLocation, releaseData, categories, workItems, pbi);
             return message;
         }
 
-        public async Task<DownloadedItems> DownloadData (string tfsProject, string branch, string changesetFrom, string changesetTo, string iteration,
-            bool includeTfsService = false) {
+        public async Task<DownloadedItems> DownloadData(string tfsProject, string branch, string changesetFrom, string changesetTo, string iteration,
+            bool includeTfsService = false)
+        {
             var queryLocation = $"$/{tfsProject}/{branch}";
-            var downloadedData = await Task.Run (() => _tfs.GetChangesetsRest (queryLocation, changesetFrom, changesetTo));
-            downloadedData.FilterTfsChanges (includeTfsService);
-            var reg = new Regex (@".*\[((\d*\,)*?(\d*))\].*");
-            var changesetWorkItemsId = downloadedData.Changes.Where (x => reg.Match (x.comment).Success)
-                .Select (x => reg.Match (x.comment).Groups[1].Captures[0].Value).Select (x => x.Split (',')).SelectMany (x => x)
-                .Select (x => Convert.ToInt32 (x)).ToList ();
-            downloadedData.WorkItems = _tfs.GetWorkItemsAdo (iteration, changesetWorkItemsId);
+            var downloadedData = await Task.Run(() => _tfs.GetChangesetsRest(queryLocation, changesetFrom, changesetTo));
+            downloadedData.FilterTfsChanges(includeTfsService);
+            var reg = new Regex(@".*\[((\d*\,)*?(\d*))\].*");
+            var changesetWorkItemsId = downloadedData.Changes.Where(x => reg.Match(x.comment).Success)
+                .Select(x => reg.Match(x.comment).Groups[1].Captures[0].Value).Select(x => x.Split(',')).SelectMany(x => x)
+                .Select(x => Convert.ToInt32(x)).ToList();
+            downloadedData.WorkItems = _tfs.GetWorkItemsAdo(iteration, changesetWorkItemsId);
             return downloadedData;
         }
     }
