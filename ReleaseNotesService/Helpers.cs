eaac8707673084c316317b723e2c50545d395a33
gregorysl <gregorysl@gmail.com>
2020-06-18 12:12:44 +0200

    if qa build date or release date is empty take current date

--- ReleaseNotesService/Helpers.cs
+++ ReleaseNotesService/Helpers.cs
@@ -17,11 +17,21 @@ namespace ReleaseNotesService
         {
             return date.ToString("yyyy-MM-dd HH:mm", new CultureInfo("en-US"));
         }
+
         public static string ParseAndFormatData(this string input, string inputFormat, string outputFormat)
         {
-            return DateTime.TryParseExact(input, inputFormat, null, DateTimeStyles.None, out var parsedDate)
-                ? parsedDate.ToString(outputFormat)
-                : $"Failed to parse {input} with format {inputFormat}";
+            DateTime parsedDate;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                parsedDate = DateTime.Today;
+            }
+            else
+            {
+                var isParsed = DateTime.TryParseExact(input, inputFormat, null, DateTimeStyles.None, out parsedDate);
+                if (!isParsed) return $"Failed to parse {input} with format {inputFormat}";
+            }
+
+            return parsedDate.ToString(outputFormat);
         }
 
         public static bool Contains(this string source, string toCheck, StringComparison comp)
